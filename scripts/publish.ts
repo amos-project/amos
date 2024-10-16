feb0db16873d8c962ec6916c1f622bf7e28aeaa3
junbao <junbao@moego.pet>
2024-10-16 14:57:15 -0700

    fix: dist tag

--- scripts/publish.ts
+++ scripts/publish.ts
@@ -3,12 +3,11 @@
  * @author junbao <junbao@moego.pet>
  */
 
-import { autorun } from './utils';
+import { autorun, getPackages } from './utils';
 import { pick } from 'lodash';
 import yargs from 'yargs';
 import { version } from './version';
 import { build } from './build';
-import * as fs from 'fs-extra';
 import * as child_process from 'node:child_process';
 import * as process from 'node:process';
 
@@ -21,12 +20,16 @@ export const publish = autorun(
           .options({
             updateVersion: {
               type: 'string',
-              choices: ['major', 'minor', 'patch', 'beta', 'none'],
-              default: 'none',
+              choices: ['major', 'minor', 'patch', 'beta'],
+              default: 'beta',
             },
             withBuild: {
               type: 'boolean',
               default: true,
+            },
+            unpublish: {
+              type: 'string',
+              default: false,
             },
           })
           .help()
@@ -34,18 +37,31 @@ export const publish = autorun(
           .parseSync(),
         'updateVersion',
         'withBuild',
+        'unpublish',
       ),
     ] as const,
-  async ({ updateVersion, withBuild }) => {
-    if (updateVersion !== 'none') {
-      await version({ update: updateVersion });
+  async ({ updateVersion, withBuild, unpublish }) => {
+    await version({ update: updateVersion });
+    const pkgs = await getPackages();
+    if (unpublish) {
+      for (const pkg of pkgs.slice()) {
+        console.log('Unpublishing ', pkg);
+        child_process.execSync(
+          `npm deprecate ${pkg}@${unpublish} 'bad version' --registry https://registry.npmjs.com`,
+          {
+            stdio: 'inherit',
+            cwd: process.cwd() + '/packages/' + pkg,
+          },
+        );
+      }
+      return;
     }
     if (withBuild) {
       await build();
     }
-    const pkgs = await fs.readdir('packages');
     for (const pkg of pkgs) {
-      child_process.execSync(`npm publish --registry https://registry.npmjs.com`, {
+      const tag = updateVersion === 'beta' ? 'beta' : 'latest';
+      child_process.execSync(`npm publish --registry https://registry.npmjs.com --tag ${tag}`, {
         stdio: 'inherit',
         cwd: process.cwd() + '/packages/' + pkg,
       });
--- scripts/utils.ts
+++ scripts/utils.ts
@@ -3,6 +3,8 @@
  * @author junbao <junbao@moego.pet>
  */
 
+import * as fs from 'fs-extra';
+
 export const autorun = <A extends any[], R>(
   module: NodeModule,
   args: () => A,
@@ -12,4 +14,27 @@ export const autorun = <A extends any[], R>(
     Promise.resolve(args()).then((args) => fn(...args));
   }
   return fn;
+};
+
+export const getPackages = async () => {
+  const pkgs = await fs.readdir('packages');
+  const tree: Record<string, string[]> = {};
+  for (const pkg of pkgs) {
+    const pj = await fs.readJSON(`packages/${pkg}/package.json`);
+    tree[pkg] = Object.keys(pj.dependencies);
+  }
+  const out: string[] = [];
+  while (true) {
+    const items = Object.entries(tree);
+    if (!items.length) {
+      break;
+    }
+    for (const [k, v] of items) {
+      if (!v.some((v) => v.startsWith('amos') && !out.includes(v))) {
+        out.push(k);
+        delete tree[k];
+      }
+    }
+  }
+  return out;
 };
