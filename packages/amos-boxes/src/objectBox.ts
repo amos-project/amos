f835c9ac11d1e91a8abc6d56342b9e003cb8ef8b
junbao <junbao@moego.pet>
2024-11-03 00:38:28 -0700

    feat: merge mergeState

--- packages/amos-boxes/src/objectBox.ts
+++ packages/amos-boxes/src/objectBox.ts
@@ -7,8 +7,7 @@ import { Box, Mutation, Selector } from 'amos-core';
 import { resolveFuncValue, shallowEqual, type ValueOrFunc } from 'amos-utils';
 
 export interface ObjectBox<T extends object> extends Box<Readonly<T>> {
-  mergeState(state: Partial<T>): Mutation<Readonly<T>>;
-  mergeState(next: (state: Readonly<T>) => Partial<T>): Mutation<Readonly<T>>;
+  mergeState(state: ValueOrFunc<Partial<Readonly<T>>, [Readonly<T>]>): Mutation<Readonly<T>>;
   set<K extends keyof T>(key: K, value: T[K]): Mutation<T>;
   get<K extends keyof T>(key: K): Selector<[K], T[K]>;
   pick<Ks extends Array<keyof T>>(...keys: Ks): Selector<Ks, Pick<T, Ks[number]>>;
